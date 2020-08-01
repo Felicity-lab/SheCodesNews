08cb74228d9177299b7aee1c9c326fb3fcc0da31
Felicity-lab <ftahwa01@gmail.com>
2020-08-01 12:59:52 +0800

    update project3

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -6,3 +6,7 @@ class NewsStory(models.Model):
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     content = models.TextField()
+
+    img_url=models.URLField(max_length=200, blank=True, null=True)
+    # def __str__(self):
+    #     return self.title + str(self.pub_date)
